8e784740c04d39f48ec5f2f86848dd71b159f986
unknown <natiahcalvinits@gmail.com>
2021-04-17 06:37:43 -0700

    modified auth page

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -4,25 +4,27 @@ import {Form, Col, Button, Card} from 'react-bootstrap';
 import getStyle from '../components/utils/bootstrapStyle';
 const AuthPage = () => {
     return (
-        <Card style={{... getStyle("card"), maxWidth: '600px'}}>
-            <Card.Body>
-                <Form>
-                    <Form.Group controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="text" placeholder="Your email" />
-                    </Form.Group>
+        <div className="container">
+            <Card style={{... getStyle("card"), maxWidth: '600px'}}>
+                <Card.Body>
+                    <Form>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="text" placeholder="Your email" />
+                        </Form.Group>
 
-                    <Form.Group controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Author" />
-                    </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Author" />
+                        </Form.Group>
 
-                    <Button variant="success" type="submit">
-                        Login
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
+                        <Button variant="success" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
 
