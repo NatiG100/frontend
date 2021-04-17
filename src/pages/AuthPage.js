5a025c68ac4d0de0a885e951af97e50ee3651c36
unknown <natiahcalvinits@gmail.com>
2021-04-17 06:36:22 -0700

    added a basic auth page

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+
+import {Form, Col, Button, Card} from 'react-bootstrap';
+import getStyle from '../components/utils/bootstrapStyle';
 const AuthPage = () => {
     return (
-        <h1> This is from the Auth Pages</h1>
+        <Card style={{... getStyle("card"), maxWidth: '600px'}}>
+            <Card.Body>
+                <Form>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" placeholder="Your email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Author" />
+                    </Form.Group>
+
+                    <Button variant="success" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 };
 
