48fbb42a1d2dee26eab2b23b14480d3e6047ec62
unknown <natiahcalvinits@gmail.com>
2021-04-17 07:05:39 -0700

    added a login and sign up switch for the auth form

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Form, Col, Button, Card} from 'react-bootstrap';
+import {Form, Button, Card} from 'react-bootstrap';
 import getStyle from '../components/utils/bootstrapStyle';
 const AuthPage = () => {
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
+const clickHandleCreate = (event) => {
+    setIsLoginMode(false);
+};
+const clickHandlerHave = (event) => {
+    setIsLoginMode(true);
+}
     return (
         <div className="container">
             <Card style={{... getStyle("card"), maxWidth: '600px'}}>
                 <Card.Body>
                     <Form>
+                        { !isLoginMode &&
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Your name" />
+                        </Form.Group>
+                        }
                         <Form.Group controlId="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="text" placeholder="Your email" />
@@ -15,13 +29,27 @@ const AuthPage = () => {
 
                         <Form.Group controlId="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Author" />
+                            <Form.Control type="password" placeholder="Your password" />
                         </Form.Group>
 
-                        <Button variant="success" type="submit">
-                            Login
-                        </Button>
-                        <Button variant="link" className="center">Already have an account</Button>
+                        {isLoginMode&&
+                            <Button variant="success" type="submit">
+                                Login
+                            </Button>
+                        }
+                        {!isLoginMode&&
+                            <Button variant="success" type="submit">
+                                Sign Up
+                            </Button>
+                        }
+                        <Form.Group controlId="switch" className="center">
+                            {isLoginMode&&
+                                <Button onClick={clickHandleCreate}variant="link">Create an account</Button>
+                            }
+                            {!isLoginMode&&
+                                <Button onClick={clickHandlerHave}variant="link">Already have an account</Button>
+                            }
+                        </Form.Group>
                     </Form>
                 </Card.Body>
             </Card>
